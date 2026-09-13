98690c2796a6bfe70e50ec649e730bdcf3bb6bd8
jlucasbx <jlucasb16@gmail.com>
2026-09-13 19:37:23 -0300

    chore: add python lexer module boilerplate

---
+++ src/tontoc/lexer/__init__.py
@@ -0,0 +1,13 @@
+from tontoc.lexer.errors import LexicalError
+from tontoc.lexer.lexer import Lexer
+from tontoc.lexer.reporter import LexerReporter
+from tontoc.lexer.rules import LexerRules
+from tontoc.lexer.types import Token
+
+__all__ = [
+    "Lexer",
+    "LexerReporter",
+    "LexerRules",
+    "LexicalError",
+    "Token",
+]
